51e34b3db3e6fb8c139e4b95e05116fd4fd8c3ab
jorgesr92 <jorgesr92@github.com>
2020-10-06 20:19:09 +0200

    first commit - class 2020.10.06

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-const URL =
-  "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
+const URL = "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
+//const URL = "https://fakestoreapi.com/products";
+
+
+const createRow = () => {
+  let divRow = document.createElement("div");
+  divRow.classList = "row";
+  app.appendChild(divRow);
+  return divRow;
+  
+}
+
+const createColumn = (divRow) => {
+  let divColumn = document.createElement("div");
+  divColumn.classList = "column";
+  divRow.appendChild(divColumn);
+  return divColumn;
+}
+
+const createImg = (divColumn, photo) => {
+  let imgPhoto = document.createElement("img");
+  imgPhoto.src = photo.url;
+  divColumn.appendChild(imgPhoto);
+}
+
+const orderPhotos = (photos) => {
+  let divRow;
+  let divColumn;
+  photos.forEach(photo => {
+    if (photos.indexOf(photo)%4 === 0) {
+      divRow = createRow();
+      divColumn = createColumn(divRow);
+      createImg(divColumn, photo);
+    } else {
+      divColumn = createColumn(divRow);
+      createImg(divColumn, photo);
+    }
+  })
+}
+
+
+const getPhotos = URL => {
+  fetch(URL).then(response => onSuccesResponse(response)).catch(error => onErrorResponse (error))
+}
+
+const onSuccesResponse = response => response.json().then(photos => {
+  orderPhotos(photos);
+});
+const onErrorResponse = error => console.error(`Aqui esta el error ${error}`);
+
+getPhotos(URL);
 
 /*
 fetch('url')
@@ -12,4 +61,10 @@ fetch('url').then(ResolveFuntion, RejectFuntion)
 
 */
 
+
 const app = document.querySelector("#app");
+
+
+
+
+
