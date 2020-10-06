4e6da01ecf9675207d1393f1d02ff45d620331a6
jorgesr92 <jorgesr92@github.com>
2020-10-06 22:16:33 +0200

    add cards for img

--- app.js
+++ app.js
@@ -2,6 +2,52 @@ const URL = "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api
 //const URL = "https://fakestoreapi.com/products";
 
 
+const createCard = (divColumn, photo) => {
+
+  let fecha = new Date(photo.pubdate);
+  let dia = fecha.getUTCDate();
+  let mes = fecha.getUTCMonth();
+  let anyo = fecha.getUTCFullYear();
+
+  let divCard = document.createElement("div");
+  divCard.classList = "card";
+  divCard.style.marginTop = "1rem";
+
+  let divBodyCard = document.createElement("div");
+  divBodyCard.classList = "card-body";
+
+  let cardTitle = document.createElement("h5");
+  cardTitle.classList = "card-title";
+  cardTitle.innerText = photo.title;
+
+  let pCard = document.createElement("p");
+  pCard.classList = "card-text";
+  pCard.innerText = photo.author;
+
+  let dateCard = document.createElement("p");
+  dateCard.classList = "card-text";
+
+  let smallDate = document.createElement("small");
+  smallDate.classList = "text-muted";
+  smallDate.innerText = `${dia}-${mes}-${anyo}`
+
+  let imgPhoto = document.createElement("img");
+  imgPhoto.classList = "card-img-top";
+  imgPhoto.style.marginTop = "0px";
+  imgPhoto.src = photo.url;
+
+
+  divColumn.appendChild(divCard);
+  divCard.appendChild(imgPhoto);
+  divCard.appendChild(divBodyCard);
+  divBodyCard.appendChild(cardTitle);
+  divBodyCard.appendChild(pCard);
+  divBodyCard.appendChild(dateCard);
+  dateCard.appendChild(smallDate);
+
+}
+
+
 const createRow = () => {
   let divRow = document.createElement("div");
   divRow.classList = "row";
@@ -17,11 +63,6 @@ const createColumn = (divRow) => {
   return divColumn;
 }
 
-const createImg = (divColumn, photo) => {
-  let imgPhoto = document.createElement("img");
-  imgPhoto.src = photo.url;
-  divColumn.appendChild(imgPhoto);
-}
 
 const orderPhotos = (photos) => {
   let divRow;
@@ -30,10 +71,10 @@ const orderPhotos = (photos) => {
     if (photos.indexOf(photo)%4 === 0) {
       divRow = createRow();
       divColumn = createColumn(divRow);
-      createImg(divColumn, photo);
+      createCard(divColumn, photo);
     } else {
       divColumn = createColumn(divRow);
-      createImg(divColumn, photo);
+      createCard(divColumn, photo);
     }
   })
 }
@@ -49,18 +90,6 @@ const onSuccesResponse = response => response.json().then(photos => {
 const onErrorResponse = error => console.error(`Aqui esta el error ${error}`);
 
 getPhotos(URL);
-
-/*
-fetch('url')
-.then(Resolve)
-.then(Reject) OR .catch(Reject)
-
-OR directly 
-
-fetch('url').then(ResolveFuntion, RejectFuntion)
-
-*/
-
 
 const app = document.querySelector("#app");
 
